ee50216da5710d409aeea45af85d15d3fc4b36ea
ahmedalhindi <alhindi.ahmed@gmail.com>
2021-05-10 22:08:47 +0300

    ping route

--- routes/root.js
+++ routes/root.js
@@ -2,4 +2,7 @@ export default async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     return { root: true }
   })
+  fastify.get('/ping', async function (request, reply) {
+    return { ping: "pong" }
+  })
 }
